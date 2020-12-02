2c01a04257f4ded26543cee01c690947ae28389c
danielliao22 <43585541+danielliao22@users.noreply.github.com>
2020-12-02 02:48:17 -0800

    Custom Skull block item added; without working texture

--- TheCoolerDaniel/LiveFaces/src/main/java/com/thecoolerdaniel/livefaces/util/RegistryHandler.java
+++ TheCoolerDaniel/LiveFaces/src/main/java/com/thecoolerdaniel/livefaces/util/RegistryHandler.java
@@ -2,7 +2,12 @@ package com.thecoolerdaniel.livefaces.util;
 
 import com.thecoolerdaniel.livefaces.LiveFaces;
 import com.thecoolerdaniel.livefaces.items.ItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.SkullBlock;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SkullItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,12 +16,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegistryHandler {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LiveFaces.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, LiveFaces.MOD_ID);
 
     public static void init() {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     // Items
     public static final RegistryObject<Item> HAPPY_FACE = ITEMS.register("happy_face", ItemBase::new);
 
+    // Skull Blocks
+    public static final RegistryObject<SkullBlock> SKULL_BLOCK = BLOCKS.register("skull_block", () -> new SkullBlock(SkullBlock.Types.PLAYER, SkullBlock.Properties.create(Material.MISCELLANEOUS)));
+    public static final RegistryObject<SkullItem> SKULL_ITEM = ITEMS.register("skull_item", () -> new SkullItem(SKULL_BLOCK.get(), SKULL_BLOCK.get(), new Item.Properties().group(ItemGroup.COMBAT)));
+
 }
+
+//SKULL BLOCK -> AbstractSkullBlock -> ContainerBlock -> Block -> AbstractBlock
+//SkULL ITEM -> WallorFloorItem -> BlockItem -> Item
