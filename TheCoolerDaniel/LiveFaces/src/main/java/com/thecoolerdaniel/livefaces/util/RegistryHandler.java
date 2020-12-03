54ef2d3d7cc03046491c80aa107e727c9e1ef26d
dpwalton <dpwalton@ucsc.edu>
2020-12-03 00:14:29 -0800

    added in happy face helmet

--- TheCoolerDaniel/LiveFaces/src/main/java/com/thecoolerdaniel/livefaces/util/RegistryHandler.java
+++ TheCoolerDaniel/LiveFaces/src/main/java/com/thecoolerdaniel/livefaces/util/RegistryHandler.java
@@ -1,10 +1,13 @@
 package com.thecoolerdaniel.livefaces.util;
 
 import com.thecoolerdaniel.livefaces.LiveFaces;
+import com.thecoolerdaniel.livefaces.armor.ModArmorMaterial;
 import com.thecoolerdaniel.livefaces.items.ItemBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.SkullBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SkullItem;
@@ -29,6 +32,9 @@ public class RegistryHandler {
     // Skull Blocks
     public static final RegistryObject<SkullBlock> SKULL_BLOCK = BLOCKS.register("skull_block", () -> new SkullBlock(SkullBlock.Types.PLAYER, SkullBlock.Properties.create(Material.MISCELLANEOUS)));
     public static final RegistryObject<SkullItem> SKULL_ITEM = ITEMS.register("skull_item", () -> new SkullItem(SKULL_BLOCK.get(), SKULL_BLOCK.get(), new Item.Properties().group(ItemGroup.COMBAT)));
+
+    // Armor
+    public static final RegistryObject<ArmorItem> FACE1 = ITEMS.register("face1", () -> new ArmorItem(ModArmorMaterial.FACE1, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)));
 
 }
 
